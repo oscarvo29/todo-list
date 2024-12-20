fcfeff39aff8302692466c3811558716aa3d2b88
oscarvo29 <oscar.vinther@gmail.com>
2024-12-20 20:45:49 +0100

    Added basic logic for handling commands

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"slices"
 )
 
+var commands = []string{"add", "delete", "list", "complete"}
+
 func main() {
-	fmt.Println("Hello world")
+	args := os.Args
+
+	if len(args) <= 1 {
+		return
+	}
+
+	for _, command := range commands {
+		if slices.Contains(args, command) {
+			switch command {
+			case "add":
+				fmt.Println("Added")
+			case "delete":
+				fmt.Println("Deleted")
+			case "list":
+				fmt.Println("Listing tasks")
+			case "complete":
+				fmt.Println("task is completed")
+			}
+		}
+	}
 }
